59d1110f1c39a3de39f3d1387ced9ec0ad752f24
victan01 <victan01@iba.dk>
2024-04-20 17:48:12 +0200

    menubar done

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+/* Menubar*/
+const menubtn = document.getElementById("menu-btn")
+const sidebar = document.getElementById("sidebar")
+const content = document.querySelector("menucontent")
+
+menubtn.addEventListener("click",()=>{
+    if(sidebar.style.left === "-250px" || sidebar.style.left === '') {
+       sidebar.style.left="0";
+       content.style.marginleft = "200px";
+    } else{
+        sidebar.style.left ="-250px";
+        content.style.marginleft= "0";
+
+    }
+});
+
+/*Faktakasser (counter)*/
 let valueDisplays = document.querySelectorAll(".fakta");
 let interval = 1500;
 
