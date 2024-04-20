dfaf207b86220496c6eb5f904a46b3cef39174bf
victan01 <victan01@iba.dk>
2024-04-20 15:23:06 +0200

    counterbox java done

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 let valueDisplays = document.querySelectorAll(".fakta");
-let interval = 100;
+let interval = 1500;
+
 
 valueDisplays.forEach((valueDisplays)=>{
     let startvalue = 0;
     let endvalue = parseInt(valueDisplays.getAttribute("data-val"));
     console.log(endvalue);
+    let duration = Math.floor (interval/endvalue);
+    let counter = setInterval(function() {
+        startvalue += 1;
+        valueDisplays.textContent = startvalue + "%";
+        if(startvalue == endvalue) {
+            clearInterval(counter)
+        }
+    }, duration);
 });
