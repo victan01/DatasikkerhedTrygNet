340e7fe514340f9969b50f385b76043adcb16529
victan01 <victan01@iba.dk>
2024-04-20 15:08:44 +0200

    java in the making

---
+++ script.js
@@ -0,0 +1,8 @@
+let valueDisplays = document.querySelectorAll(".fakta");
+let interval = 100;
+
+valueDisplays.forEach((valueDisplays)=>{
+    let startvalue = 0;
+    let endvalue = parseInt(valueDisplays.getAttribute("data-val"));
+    console.log(endvalue);
+});
