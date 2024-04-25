8eb0f74867bcd2b776e4ff5d2d87cd094fd06329
victan01 <victan01@iba.dk>
2024-04-25 17:37:17 +0200

    Some jaaaaavvvva for uu ahaha

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ valueDisplays.forEach((valueDisplays)=>{
 
 /*Sikkerhedspillet*/
 /* Jeg kalder først på mine ID*/
-
 const StartKnap = document.getElementById('start-knap')
 const SpørgsmålsBox = document.getElementById('spørgsmålsbox')
 const SpørgsmålsTekst = document.getElementById('spørgsmålstekst')
@@ -53,3 +52,28 @@ const Forkertsvar = document.getElementById('forkertsvar')
 const Rigtigtsvar = document.getElementById('rigtigtsvar')
 const Vinderlyd = document.getElementById('vinderlyd')
 const Taberlyd = document.getElementById('taberlyd')
+
+/* Score variabler*/
+Let.currentQuestionIndex = 0; 
+Let.score = 0;
+
+/* Dernæst påbegynder jeg mine spørgsmål med svarmuligheder*/
+ const Spørgsmål = [
+  {
+    question: 'Anna er på udgik efter sin drømme computer og finder en butik, som sælger den til en meget lav pris',
+    opstions: {'Klikke på "Køb nu" og købe den med det samme', 'Undersøge butikken nærmere for at sikre sig, at den er troværdig' }
+    ,correctAnswerIndex = 1
+  }
+
+  {
+    question: 'Anna støder på en online butik med profesionelt design og meget positive anmeldelser',
+    opstions: {'Stole på butikkens troværdighed og foretage købet med det samme', 'Undersøge ydligere for at sikre at butikken er troværdig' }
+    ,correctAnswerIndex = 1
+  }
+
+  {
+    question: 'Anna støder på en online butik med profesionelt design og meget positive anmeldelser',
+    opstions: {'Stole på butikkens troværdighed og foretage købet med det samme', 'Undersøge ydligere for at sikre at butikken er troværdig' }
+    ,correctAnswerIndex = 1
+  }
+];
